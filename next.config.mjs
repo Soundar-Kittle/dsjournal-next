5368a8a570256257992cd02274f2b7f058d60a87
Soundararajan <soundar.kittle@gmail.com>
2025-10-02 15:17:41 +0530

    pageloadminimize

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
+// next.config.mjs
+import bundleAnalyzer from "@next/bundle-analyzer";
+
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
