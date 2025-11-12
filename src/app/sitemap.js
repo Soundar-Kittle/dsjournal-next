0ea6926cc6a23d8d76b1593cb29155f8ab5f813e
kittle-balaguru <kittle.balaguru@gmail.com>
2025-11-12 19:25:18 +0530

    Refactor sitemap function to include journals and improve URL construction

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,24 +1,33 @@
-// import { getStaticRoutes } from "@/utils";
+import { getStaticRoutes } from "@/utils/getStaticRoutes";
+import { getJournals } from "@/utils/journals";
 
-// export default async function sitemap() {
-//   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://demo.dsjournals.com";
+export default async function sitemap() {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_BASE_URL || "https://demo.dreamscience.com";
 
-//   const routes = getStaticRoutes();
-//   const services = await getServices();
+  const routes = await getStaticRoutes();
 
-//   const staticEntries = routes.map(({ url }) => ({
-//     url: `${baseUrl}${url}`,
-//     lastModified: new Date().toISOString(),
-//     changeFrequency: "weekly",
-//     priority: url === "/" ? 1.0 : 0.7,
-//   }));
+  const staticEntries = routes.map(({ url }) => ({
+    url: `${baseUrl}/${url}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: "weekly",
+    priority: url === "/" ? 1.0 : 0.7,
+  }));
 
-//   const serviceEntries = services.map((s) => ({
-//     url: `${baseUrl}/services/${s.slug}`,
-//     lastModified: new Date().toISOString(),
-//     changeFrequency: "weekly",
-//     priority: 0.8,
-//   }));
+  // const journals = await getJournals();
 
-//   return [...staticEntries, ...serviceEntries];
-// }
+  // const journalsEntries = journals.map((s) => ({
+  //   url: `${baseUrl}/${s.slug}`,
+  //   lastModified: new Date().toISOString(),
+  //   changeFrequency: "weekly",
+  //   priority: 0.8,
+  // }));
+
+  return [
+    ...staticEntries,
+    {
+      url: `${baseUrl}/journals/sitemap.xml`,
+      lastModified: new Date().toISOString(),
+    },
+  ];
+}
